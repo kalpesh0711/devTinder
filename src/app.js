fe510769b065a372f2d56befcbfa308d90223fb5
kalpesh0711 <kalpeshchoudhary202@gmail.com>
2026-02-02 23:55:51 +0530

    routes update

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 
 const app = express();
+
+app.use("/test",(req,res)=>{
+    res.send("Welcome to Node");
+
 app.use((req,res)=>{
-    res.send("Hello from server singh..");
+    res.send("Hello from server singh ji..");
+});
+
+
 });
 
 app.listen(3000,()=> {

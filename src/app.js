3d7f84968821944d4a0d6f222fafb0a4d3ec91f2
kalpesh0711 <kalpeshchoudhary202@gmail.com>
2026-02-04 19:14:09 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -2,32 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user",(req,res)=>{
-    res.send("name:kalpesh lastName:choudhary")
+app.get("/user",(req,res,next)=>{
+    //res.send("1st Response")
+    next()
 });
 
-app.post("/user",(req,res)=>{
-    res.send("data uploaded")
+app.get("/user",(req,res,next)=>{
+   // res.send("2nd Response")
+    next()
 });
 
-app.delete("/user",(req,res)=>{
-    res.send("data deleted")
+app.get("/user",(req,res,next)=>{
+    res.send("3rd Response")
+    next()
 });
 
 
-app.use("/test",(req,res)=>{
-    res.send("Welcome to test");
-});      
 
-
-
-app.use("/run",(req,res)=>{
-    res.send("Welcome to run");
-});   
-
-// app.use((req,res)=>{
-//     res.send("Hello from server singh ji..");   
-// });
 
 app.listen(3000,()=> {
     console.log("Server is successfully listening on port 3000...")
